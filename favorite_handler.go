0bb6a414e3e543a7c316c4e7986b3565d77cb67b
lxystar00 <103492886+lxystar00@users.noreply.github.com>
2023-08-23 13:02:34 +0800

    Update favorite_handler.go

--- favorite_handler.go
+++ favorite_handler.go
@@ -5,6 +5,7 @@ import (
 	"douyin/database"
 	"douyin/database/models"
 	"fmt"
+	"sync"
 	"time"
 
 	//"log"
@@ -18,12 +19,8 @@ import (
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	//"sync"
 )
-
-/*const (
-	successCode = 0
-	errorCode   = 1
-)*/
 
 //const dbName string = "mysql"
 //const dbConnect string = "root:123456@tcp(127.0.0.1:3306)/douyin?charset=utf8&parseTime=true" //设置数据库连接参数
@@ -46,16 +43,18 @@ func SuccessResponse(ctx *gin.Context) {
 	})
 }
 
+// FavActionParams是获取点赞操作请求的结构体
 type FavActionParams struct {
 	Token      string `form:"token" binding:"required"`
 	VideoId    int64  `form:"video_id" binding:"required"`
 	ActionType int8   `form:"action_type" binding:"required,oneof=1 2"`
 }
 
+// FavListResponse是获取点赞列表响应的结构体
 type FavListResponse struct {
-	StatusCode int            `json:"status_code"`
-	StatusMsg  string         `json:"status_msg"`
-	VideoList  []models.Video `json:"video_list"`
+	StatusCode int              `json:"status_code"`
+	StatusMsg  string           `json:"status_msg"`
+	VideoList  []models.VideoFA `json:"video_list"`
 }
 
 // 点赞视频
@@ -90,28 +89,55 @@ func FavoriteAction(ctx *gin.Context) {
 }
 
 func FavoriteActionDo(uid, vid int64, action int8, redisPool *redis.Pool) error {
-	var authoruid int64
-	err := database.DB.Table("video").Where("video_id = ?", vid).Pluck("author_user_id", &authoruid).Error
-	if err != nil {
-		fmt.Println("查询视频作者id失败")
+	conn := redisPool.Get() //重用已有的连接
+	defer conn.Close()
+	// 获取视频对应的用户ID
+	authoruid, _ := redis.Int64(conn.Do("HGET", "video:"+strconv.FormatInt(vid, 10), "author_user_id"))
+
+	db := database.DB.Begin() //开启数据库事务
+
+	defer func() { //协程中发生 panic 时
+		if r := recover(); r != nil {
+			db.Rollback() //回滚
+		}
+	}()
+
+	var wg sync.WaitGroup //等待所有协程完成
+	wg.Add(4)             //启动4个协程
+
+	act := action == 1 // 设置 act 为 true 或 false
+
+	go func() {
+		FavoriteTableChange(db, "favorite", uid, vid, act)
+		defer wg.Done() //表示协程已经完成了其任务
+	}()
+
+	go func() {
+		ChangeUserFavoriteCount(db, "user", uid, "favorite_count", act)
+		defer wg.Done() //表示协程已经完成了其任务
+	}()
+
+	go func() {
+		ChangeUserFavoriteCount(db, "user", authoruid, "total_favorited", act)
+		defer wg.Done() //表示协程已经完成了其任务
+	}()
+
+	go func() {
+		ChangeVideoLikesCount(db, "video", vid, act)
+		defer wg.Done() //表示协程已经完成了其任务
+	}()
+
+	wg.Wait() // 等待所有协程完成
+
+	if err := db.Commit(); err != nil { //db.Commit()提交数据库的更改
+		fmt.Println("提交数据库修改时错误", err)
 	}
 
-	if action == 1 {
-		FavoriteTableChange("favorite", uid, vid, true)
-		ChangeUserFavoriteCount("user", uid, "favorite_count", true)
-		ChangeUserFavoriteCount("user", authoruid, "total_favorited", true)
-		ChangeVideoLikesCount("video", vid, true)
-		CacheFavoriteAction(uid, vid, true, redisPool)
-	} else {
-		FavoriteTableChange("favorite", uid, vid, false)
-		ChangeUserFavoriteCount("user", uid, "favorite_count", false)
-		ChangeUserFavoriteCount("user", authoruid, "total_favorited", false)
-		ChangeVideoLikesCount("video", vid, false)
-		CacheFavoriteAction(uid, vid, false, redisPool)
+	CacheFavoriteAction(uid, vid, act, redisPool)
 
-	}
-
+	fmt.Println("All database operations completed.")
 	return nil
+
 }
 
 func CacheFavoriteAction(uid, vid int64, action bool, redisPool *redis.Pool) error {
@@ -123,90 +149,84 @@ func CacheFavoriteAction(uid, vid int64, action bool, redisPool *redis.Pool) err
 	authoruid, err := redis.Int64(conn.Do("HGET", "video:"+strconv.FormatInt(vid, 10), "author_user_id"))
 
 	if err != nil {
+		fmt.Println(err)
 		return err
 	}
 
-	//使用 Redis 的字符串（String）来存储用户表和视频表的信息，使用集合（Set）来存储点赞关系。
+	if redisPool != nil {
+		fmt.Println("Get conn!")
+	}
+	//使用 Redis 的哈希（Hash）来存储用户表和视频表的信息，使用集合（Set）来存储点赞关系。
 
 	if action {
-		conn.Send("SADD", "user:"+strconv.FormatInt(uid, 10)+":likes", vid)            //存储点赞关系
-		conn.Send("INCR", "user:"+strconv.FormatInt(uid, 10)+":favorite_count")        //增加用户点赞数
-		conn.Send("INCR", "video:"+strconv.FormatInt(vid, 10)+":likes_count")          //增加用户点赞量
-		conn.Send("INCR", "user:"+strconv.FormatInt(authoruid, 10)+":total_favorited") //增加视频作者的被点赞量
-
+		conn.Send("SADD", "user:"+strconv.FormatInt(uid, 10)+":likes", vid)                  //存储点赞关系
+		conn.Send("HINCRBY", "user:"+strconv.FormatInt(uid, 10), "favorite_count", 1)        //增加用户点赞数
+		conn.Send("HINCRBY", "video:"+strconv.FormatInt(vid, 10), "likes_count", 1)          //增加用户点赞量
+		conn.Send("HINCRBY", "user:"+strconv.FormatInt(authoruid, 10), "total_favorited", 1) //增加视频作者的被点赞量
+		fmt.Println("cache ok!")
 	} else {
 		conn.Send("SREM", "user:"+strconv.FormatInt(uid, 10)+":likes", vid)
-		conn.Send("DECR", "user:"+strconv.FormatInt(uid, 10)+":favorite_count")
-		conn.Send("DECR", "video:"+strconv.FormatInt(vid, 10)+":likes_count")
-		conn.Send("DECR", "user:"+strconv.FormatInt(authoruid, 10)+":total_favorited")
+		conn.Send("HINCRBY", "user:"+strconv.FormatInt(uid, 10), "favorite_count", -1)
+		conn.Send("HINCRBY", "video:"+strconv.FormatInt(vid, 10), "likes_count", -1)
+		conn.Send("HINCRBY", "user:"+strconv.FormatInt(authoruid, 10), "total_favorited", -1)
+		fmt.Println("cache ok!")
 	}
 	conn.Flush()
 	return nil
 }
 
-func FavoriteTableChange(tableName string, userID int64, videoID int64, action bool) {
-	/*db, err := sql.Open(dbName, dbConnect)
-	if err != nil {
-		fmt.Println("数据库连接失败")
-		return
-	}
-	defer db.Close()
-	*/
-	if action {
-		// 根据用户id和视频id在点赞关系表中查询对应的结果
-		//checkExistSql := fmt.Sprintf("SELECT COUNT(*) FROM %s WHERE user_id=%d AND video_id=%d", tableName, userID, videoID)
-		var count int
-		//err := db.QueryRow(checkExistSql).Scan(&count)
-		err := database.DB.Table(tableName).Where("user_id=? AND video_id=?", userID, videoID).Count(&count).Error
-		if err != nil {
-			fmt.Println("查询记录失败:", err)
-			return
-		}
+// 更新favorite表，1代表取消点赞，-1代表未取消点赞
+func FavoriteTableChange(db *gorm.DB, tableName string, userID int64, videoID int64, action bool) {
+	var fav models.Favorite
+	err := db.Table(tableName).Where("user_id = ? AND video_id = ?", userID, videoID).First(&fav).Error
 
-		if count > 0 {
-			// 存在则删除
-			//deleteSql := fmt.Sprintf("DELETE FROM %s WHERE user_id=%d AND video_id=%d", tableName, userID, videoID)
-			//_, err := db.Exec(deleteSql)
-			err := database.DB.Table(tableName).Where("user_id=? AND video_id=?", userID, videoID).Delete(&models.Favorite{}).Error
+	now := time.Now()
+
+	if action {
+		if gorm.IsRecordNotFoundError(err) {
+			// 插入新记录，is_deleted 为 0，created_at 和 update_at 为当前时间
+			newFav := models.Favorite{
+				UserID:    userID,
+				VideoID:   videoID,
+				IsDeleted: -1,
+				CreatedAt: now,
+				UpdatedAt: now,
+			}
+			err = db.Table(tableName).Create(&newFav).Error
 			if err != nil {
-				fmt.Println("删除记录失败:", err)
+				fmt.Println("插入记录失败:", err)
 				return
 			}
-		}
-
-		fav := models.Favorite{
-			UserID:    userID,
-			VideoID:   videoID,
-			CreatedAt: time.Now(),
-		}
-		// 增加
-		//insertSql := fmt.Sprintf("INSERT INTO %s (user_id, video_id, created_at) VALUES (%d, %d, NOW())", tableName, userID, videoID)
-		//_, err = db.Exec(insertSql)
-		err = database.DB.Table(tableName).Create(&fav).Error
-		if err != nil {
-			fmt.Println("插入记录失败:", err)
-			return
+			//fmt.Println("插入成功")
+		} else {
+			// 更新记录的 is_deleted 和 updated_at 字段
+			err = db.Model(&fav).Updates(models.Favorite{IsDeleted: -1, UpdatedAt: now}).Error
+			if err != nil {
+				fmt.Println("更新记录失败:", err)
+				return
+			}
+			//fmt.Println("点赞更改记录成功")
 		}
 	} else {
-		// 删除
-		//deleteSql := fmt.Sprintf("DELETE FROM %s WHERE user_id=%d AND video_id=%d", tableName, userID, videoID)
-		//_, err := db.Exec(deleteSql)
-		err := database.DB.Table(tableName).Where("user_id=? AND video_id=?", userID, videoID).Delete(&models.Favorite{}).Error
-		if err != nil {
-			fmt.Println("删除记录失败:", err)
-			return
+		if gorm.IsRecordNotFoundError(err) {
+			if err != nil {
+				fmt.Println("操作错误:", err)
+				return
+			}
+		} else {
+			// 更新记录的 is_deleted 和 updated_at 字段
+			err = db.Model(&fav).Updates(models.Favorite{IsDeleted: 1, UpdatedAt: now}).Error
+			if err != nil {
+				fmt.Println("更新记录失败:", err)
+				return
+			}
+			//fmt.Println("取消点赞更改记录成功")
 		}
 	}
 }
 
-// 修改用户的 favorite_count
-func ChangeUserFavoriteCount(tableName string, userID int64, userORauther string, action bool) {
-	/*db, err := sql.Open(dbName, dbConnect)
-	if err != nil {
-		fmt.Println("数据库连接失败")
-		return
-	}*/
-
+// 修改用户的 favorite_count 及 total_favorited
+func ChangeUserFavoriteCount(db *gorm.DB, tableName string, userID int64, userORauther string, action bool) {
 	var changeValue int
 	if action {
 		changeValue = 1
@@ -217,7 +237,7 @@ func ChangeUserFavoriteCount(tableName string, userID int64, userORauther string
 	//Sql := fmt.Sprintf("update %s set favorite_count = favorite_count + %d where id = %d", tableName, changeValue, userID)
 	//_, err = db.Exec(Sql)
 	//err := database.DB.Table(tableName).Where("id=?", userID).Update("favorite_count", gorm.Expr("favorite_count+?", changeValue)).Error
-	err := database.DB.Table(tableName).Where("id=?", userID).Update(userORauther, gorm.Expr(userORauther+" + ?", changeValue)).Error
+	err := db.Table(tableName).Where("id=?", userID).Update(userORauther, gorm.Expr(userORauther+" + ?", changeValue)).Error
 	if err != nil {
 		fmt.Println("更新 favorite_count 失败:", err)
 	}
@@ -226,13 +246,7 @@ func ChangeUserFavoriteCount(tableName string, userID int64, userORauther string
 }
 
 // 修改视频的 likes_count
-func ChangeVideoLikesCount(tableName string, videoID int64, action bool) {
-	/*db, err := sql.Open(dbName, dbConnect)
-	if err != nil {
-		fmt.Println("数据库连接失败")
-		return
-	}*/
-
+func ChangeVideoLikesCount(db *gorm.DB, tableName string, videoID int64, action bool) {
 	var changeValue int
 	if action {
 		changeValue = 1
@@ -242,7 +256,7 @@ func ChangeVideoLikesCount(tableName string, videoID int64, action bool) {
 
 	//Sql := fmt.Sprintf("update %s set likes_count = likes_count + %d where video_id = %d", tableName, changeValue, videoID)
 	//_, err = db.Exec(Sql)
-	err := database.DB.Table(tableName).Where("video_id=?", videoID).Update("likes", gorm.Expr("likes+?", changeValue)).Error
+	err := db.Table(tableName).Where("id=?", videoID).Update("likes", gorm.Expr("likes+?", changeValue)).Error
 	if err != nil {
 		fmt.Println("更新 likes_count 失败:", err)
 	}
@@ -256,14 +270,8 @@ func FavoriteList(ctx *gin.Context) {
 	//token := ctx.Query("token")
 	userID, _ := strconv.ParseInt(uID, 10, 64)
 
-	// 验证token是否有效
-	/*if err := validateToken(userID, token); err != nil {
-		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "无效的Token"})
-		return
-	}*/
-
 	videoId := GetVideoId(userID)
-	fmt.Println("videoId==", videoId)
+	//fmt.Println("videoId==", videoId)
 	resp, err := GetVideoById(videoId)
 	if err != nil {
 		fmt.Println("出错了，获取视频失败")
@@ -284,39 +292,50 @@ func FavoriteList(ctx *gin.Context) {
 // 获取视频id
 func GetVideoId(userID int64) []int64 {
 	var videoId []int64
-	//db, _ := sql.Open(dbName, dbConnect)
-	/*sql := fmt.Sprintf("select video_id from favorite where user_id=%d", userID)
-	rows, err := database.DB.Query(sql)
-	if err != nil {
-		log.Println(err)
-		return videoId, err
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var vid int64
-		// 获取各列的值，放到对应的地址中
-		rows.Scan(&vid)
-		videoId = append(videoId, vid)
-	}*/
-	//defer db.Close()
 	fmt.Println(userID)
-	err := database.DB.Table("favorite").Where("user_id=?", userID).Pluck("video_id", &videoId).Error
+	err := database.DB.Table("favorite").Where("user_id=? AND is_deleted=-1", userID).Pluck("video_id", &videoId).Error
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(videoId)
+	//fmt.Println(videoId)
 	return videoId
 }
 
 // 获取视频实例
-func GetVideoById(videoId []int64) ([]models.Video, error) {
-	var videos []models.Video
+func GetVideoById(videoId []int64) ([]models.VideoFA, error) {
+	var videos []models.VideoFA
 	for _, id := range videoId {
 		fmt.Println(id)
 		var v models.Video
-		database.DB.Table("video").Where("video_id=?", id).Find(&v)
-		fmt.Println(v)
-		videos = append(videos, v)
+		var u models.User
+		database.DB.Table("video").Where("id=?", id).Find(&v)
+		//fmt.Println(v)
+		database.DB.Table("user").Where("id=?", v.AuthorUserID).Find(&u)
+		//fmt.Println(u)
+		userfa := models.UserFA{
+			Avatar:          u.Avatar,
+			BackgroundImage: u.BackgroundImage,
+			FavoriteCount:   u.FavoriteCount,
+			FollowCount:     u.FollowCount,
+			FollowerCount:   u.FollowerCount,
+			ID:              u.ID,
+			IsFollow:        false,
+			Name:            u.Name,
+			Signature:       u.Signature,
+			TotalFavorited:  strconv.FormatInt(u.TotalFavorited, 10),
+			WorkCount:       u.WorkCount,
+		}
+		videofa := models.VideoFA{
+			Author:        userfa,
+			CommentCount:  int64(v.Comments),
+			CoverURL:      v.CoverURL,
+			FavoriteCount: int64(v.Likes),
+			ID:            v.VideoID,
+			IsFavorite:    true,
+			PlayURL:       v.PlayURL,
+			Title:         v.Title,
+		}
+		videos = append(videos, videofa)
 	}
 	return videos, nil
 }
